531b9b4ed68c1d366345a6c119a8eab8d6d1c452
Sabbir <sabbir.ansari3574@gmail.com>
2026-08-20 13:59:03 +0530

    login api

--- admin/admin.js
+++ admin/admin.js
@@ -1,4 +1,4 @@
-var API_BASE_URL = window.API_BASE_URL || 'http://127.0.0.1:8000/api/v1';
+var API_BASE_URL = window.API_BASE_URL || 'https://ii.elabourpurulia.com/api/v1';
 
 let currentTab = 'companies';
 let currentPage = 1;
